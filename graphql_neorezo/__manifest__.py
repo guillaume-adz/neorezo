81ae3e3f4c9ee0625cd729bfea9b8d6566d9a279
gdoumenc <gdoumenc@fpr-coworks.com>
2021-11-14 15:32:59 +0100

    graphql from OCA

---
+++ graphql_neorezo/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2018 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "GraphQL NeoRezo",
+    "version": "0.1",
+    "license": "MIT",
+    "author": "Guillaume DOUMENC",
+    "depends": ["graphql_base"],
+    "external_dependencies": {"python": ["graphene"]},
+    "development_status": "Beta",
+    "installable": False,
+}
