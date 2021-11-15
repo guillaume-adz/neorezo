5d4c5a59d7469b928c0a0838070880fbdff2f596
gdoumenc <gdoumenc@fpr-coworks.com>
2021-11-15 18:44:34 +0100

    dev branch removed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # neorezo
 
 1. Change Administrator name with microservice@neorezo.io
-2. Generate API key for microservice access
-3. Install apps : Facturation / Comptabilité / Plan Comptable Français
+2. Change microservice@neorezo.io password
+3. Generate API key for microservice access (set developper mode)
+4. Install apps : Facturation / Comptabilité / Plan Comptable Français
+5. Install apps: NeoRezo
--- neorezo/controllers/main.py
+++ neorezo/controllers/main.py
@@ -13,7 +13,7 @@ _logger = logging.getLogger(__name__)
 class NeoRezoController(http.Controller, GraphQLControllerMixin):
 
     # The GraphiQL route, providing an IDE for developers
-    @http.route("/graphiql/neorezo", auth="apikey")
+    @http.route("/graphiql", auth="apikey")
     def graphiql(self, **kwargs):
         try:
             return self._handle_graphiql_request(schema)
@@ -24,12 +24,12 @@ class NeoRezoController(http.Controller, GraphQLControllerMixin):
     # requests. If you only need to accept GET requests or POST
     # with application/x-www-form-urlencoded content,
     # this is not necessary.
-    GraphQLControllerMixin.patch_for_json("^/graphql/neorezo/?$")
+    GraphQLControllerMixin.patch_for_json("^/graphql/?$")
 
     # The graphql route, for applications.
     # Note csrf=False: you may want to apply extra security
     # (such as origin restrictions) to this route.
-    @http.route("/graphql/neorezo", auth="apikey", csrf=False)
+    @http.route("/graphql", auth="apikey", csrf=False)
     def graphql(self, **kwargs):
         try:
             return self._handle_graphql_request(schema)
