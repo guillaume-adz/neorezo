e6baf7cff14a0baf94a6821e6d5114a7363a96b3
gdoumenc <gdoumenc@fpr-coworks.com>
2021-11-16 15:25:08 +0100

    Odoo scalar added

--- neorezo/schema/scalar.py
+++ neorezo/schema/scalar.py
@@ -1,9 +1,13 @@
+import logging
+
 from graphene import Field
 from graphene import Int
 from graphene import List
 from graphene import NonNull
 from graphene import String
 from odoo.addons.graphql_base import OdooObjectType
+
+_logger = logging.getLogger(__name__)
 
 
 def default_list_resolver(parent, info, domain=None, **kwargs):
@@ -14,12 +18,17 @@ def default_list_resolver(parent, info, domain=None, **kwargs):
 class OdooList(List):
     """A graphene List with an Odoo aware default resolver."""
 
-    def __init__(self, of_type: OdooObjectType, **kwargs):
+    def __init__(self, of_type: OdooObjectType, resolver=None, **kwargs):
+        resolver = resolver or default_list_resolver
         super().__init__(NonNull(of_type), required=True, resolver=default_list_resolver,
                          limit=Int(), offset=Int(), **kwargs)
 
 
 def record_resolver(parent, info, id, **kwargs):
+    _logger.info(parent)
+    _logger.info(info)
+    _logger.info(id)
+    _logger.info(kwargs)
     domain = [('id' '=', id)]
     return default_list_resolver(parent, info, domain=domain, **kwargs)
 
@@ -27,5 +36,6 @@ def record_resolver(parent, info, id, **kwargs):
 class OdooRecord(Field):
     """A graphene Field with an Odoo aware default resolver."""
 
-    def __init__(self, of_type: OdooObjectType, **kwargs):
+    def __init__(self, of_type: OdooObjectType, resolver=None, **kwargs):
+        resolver = resolver or record_resolver
         super().__init__(of_type, resolver=record_resolver, id=String(required=True))
