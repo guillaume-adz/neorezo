63266d07075bd54f9490d93936d30899abb8b911
gdoumenc <gdoumenc@fpr-coworks.com>
2021-11-16 12:03:52 +0100

    Odoo scalar added

--- neorezo/schema/scalar.py
+++ neorezo/schema/scalar.py
@@ -6,11 +6,16 @@ from graphene import String
 from odoo.addons.graphql_base import OdooObjectType
 
 
+def default_list_resolver(parent, info, **kwargs):
+    domain = [[]]
+    return info.context["env"][parent.odoo_model].search(domain, **kwargs)
+
+
 class OdooList(List):
     """A graphene List with an Odoo aware default resolver."""
 
     def __init__(self, of_type: OdooObjectType, odoo_model: str, **kwargs):
-        super().__init__(NonNull(of_type), required=True, resolver=self.default_resolver,
+        super().__init__(NonNull(of_type), required=True, resolver=default_list_resolver,
                          limit=Int(), offset=Int(), **kwargs)
         self.odoo_model = odoo_model
 
@@ -18,20 +23,15 @@ class OdooList(List):
     def of_type(self):
         return self._of_type
 
-    @staticmethod
-    def default_resolver(self, info, **kwargs):
-        domain = [[]]
-        return info.context["env"][self.odoo_model].search(domain, **kwargs)
+
+def default_field_resolver(parent, info, id, **kwargs):
+    domain = [('id' '=', id)]
+    return info.context["env"][parent.list.odoo_model].search(domain, **kwargs)
 
 
 class OdooRecord(Field):
     """A graphene Field with an Odoo aware default resolver."""
 
     def __init__(self, list: OdooList, **kwargs):
-        super().__init__(list.of_type, resolver=self.default_resolver, id=String(required=True))
+        super().__init__(list.of_type, resolver=self.default_field_resolver, id=String(required=True))
         self.list = list
-
-    @staticmethod
-    def default_resolver(self, info, id, **kwargs):
-        domain = [('id' '=', id)]
-        return info.context["env"][self.list.odoo_model].search(domain, **kwargs)
