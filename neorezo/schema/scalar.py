535556da3a46ee7430d3a231ab3a72a604f4f73a
gdoumenc <gdoumenc@fpr-coworks.com>
2021-11-18 18:57:32 +0100

    active added to tenants filter

--- neorezo/schema/scalar.py
+++ neorezo/schema/scalar.py
@@ -1,10 +1,8 @@
 import logging
 import typing as t
 
-from graphene import Field
 from graphene import Int
 from graphene import List
-from graphene import String
 from graphene.types.objecttype import ObjectTypeOptions
 from odoo.addons.graphql_base import OdooObjectType
 
@@ -33,10 +31,6 @@ class OdooType(OdooObjectType):
         return cls._meta.odoo_model
 
 
-def odoo_resolver(object_type: OdooType, info, domain=None, **kwargs):
-    return info.context["env"][object_type.odoo_model()].search(domain, **kwargs)
-
-
 class OdooList(List):
     """A graphene List with an Odoo aware default resolver."""
 
@@ -45,19 +39,8 @@ class OdooList(List):
         super().__init__(of_type, resolver=resolver, limit=Int(), offset=Int(), **kwargs)
 
     def record_resolver(self, parent, info, limit=50, offset=0, **kwargs):
+        _logger.error(parent._fields)
         domain = []
         for field, value in kwargs.items():
             domain.append((field, '=', value))
-        return odoo_resolver(self._of_type, info, domain=domain, limit=limit, offset=offset)
-
-
-class OdooRecord(Field):
-    """A graphene Field with an Odoo aware default resolver."""
-
-    def __init__(self, of_type: t.Type[OdooType], resolver=None, **kwargs):
-        resolver = resolver or self.record_resolver
-        super().__init__(of_type, resolver=resolver, id=String(required=True))
-
-    def record_resolver(self, parent, info, id, **kwargs):
-        domain = [('id', '=', id)]
-        return odoo_resolver(self._type, info, domain=domain, **kwargs)
+        return info.context["env"][self._of_type.odoo_model()].search(domain, limit=limit, offset=offset)
